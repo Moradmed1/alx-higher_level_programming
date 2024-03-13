04a020ba844a4a199ed8d1bc00ba6524df5072a0
Moradmed1 <Moradmed1@gmail.com>
2024-03-13 12:34:59 -0700

    d

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    print('{}'.format(chr(i + 65) if i % 2 == 0 else chr(i + 97)), end='')
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
