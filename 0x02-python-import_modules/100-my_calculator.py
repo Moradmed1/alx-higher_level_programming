6480c7524c9332040bfcbcfc1923090f8e148d04
Moradmed1 <Moradmed1@gmail.com>
2024-03-15 15:21:50 -0700

    d

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
 
-nargs = len(sys.argv) - 1
-if nargs != 3:
-print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-sys.exit(1)
+    msg = ["Usage: ./100-my_calculator.py <a> <operator> <b>",
+           "Unknown operator. Available operators: +, -, * and /"]
+    op = {'+': add, '-': sub, '*': mul, '/': div}
 
-op = sys.argv[2]
-if op != '+' and op != '-' and op != '*' and op != '/':
-print("Unknown operator. Available operators: +, -, * and /")
-sys.exit(1)
+    if len(argv) != 4 or argv[2] not in list(op.keys()):
+        print(msg[0] if len(argv) != 4 else msg[1])
+        exit(1)
 
-from calculator_1 import add, sub, mul, div
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-
-if op == '+':
-print("{} + {} = {}".format(a, b, add(a, b)))
-elif op == '-':
-print("{} - {} = {}".format(a, b, sub(a, b)))
-elif op == '*':
-print("{} * {} = {}".format(a, b, mul(a, b)))
-else:
-print("{} / {} = {}".format(a, b, div(a, b)))
+    a = int(argv[1])
+    b = int(argv[3])
+    print(f"{a} {argv[2]} {b} = {op[argv[2]](a, b)}")
